40eac35efde82ebaf1d3be171d8d2cc5a8162e12
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-28 19:50:30 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var ol = require('openlayers');
 
+
 var map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
-    })
-  ],
-  view: new ol.View({
-    center: [0, 0],
-    zoom: 0
-  })
+	target: 'map',
+	layers: [
+		new ol.layer.Tile({
+			source: new ol.source.OSM()
+	  })
+	],
+	view: new ol.View({
+		center: [latitude, longitude],
+		zoom: 4
+	})
 });
