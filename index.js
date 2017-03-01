ddd5e07183ce27969e7f8143b95d1e0761ac5922
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-28 19:44:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 var ol = require('openlayers');
 
 var map = new ol.Map({
-	target: 'map',
-	layers: [
-		new ol.layer.Tile({
-			source: new ol.source.OSM()
-	  })
-	],
-	view: new ol.View({
-		center: [latitude, longitude],
-		zoom: 4
-	})
-});
-var starship = new ol.Overlay({
-	element: document.getElementById('enterprise'),
-	offset: [-200,-200]
-});
-map.addOverlay(starship);
-starship.setPosition([latitude, longitude]);
-
-map.on('click', function(event){
-	var coords = event.coordinate;
-	starship.setPosition(coords);
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 0
+  })
 });
