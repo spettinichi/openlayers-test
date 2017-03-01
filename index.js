42565ab22782d56f7c777934365842a12461ba53
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-28 20:03:14 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ var map = new ol.Map({
 		zoom: 4
 	})
 });
+var starship = new ol.Overlay({
+	element: document.getElementById('enterprise'),
+	offset: [-200,-200]
+});
+map.addOverlay(starship);
+starship.setPosition([latitude, longitude]);
+
+map.on('click', function(event){
+	var coords = event.coordinate;
+	starship.setPosition(coords);
+});
