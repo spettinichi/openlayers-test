768efec5eb60d0a12a38e35f97e9f8b1dcbccbbb
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-03-07 18:18:36 -0500

    Removed junk

--- index.sh
+++ index.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-#COUNTER=0
-#latitude=-50
-#longitude=-100
-#while [ $COUNTER -ne "20" ]; do
-#	coords="[$latitude,$longitude]"
-#	echo $coords
-#	sleep 1
-#	COUNTER=$(( $COUNTER + 1 ))
-#	latitude=$(( $latitude + 5 ))
-#	longitude=$(( $longitude + 10 ))
-#done
-
 python3 index.py
